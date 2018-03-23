c4f4fead34c700d0f7c08ae14d1e492fb019fa79
Nicholas Hutchinson <nicholas.hutchinson@foundry.com>
2018-03-23 16:26:32 +0000

    stackcollapse-vtune.pl: skip lines with log messages

--- stackcollapse-vtune.pl
+++ stackcollapse-vtune.pl
@@ -9,7 +9,7 @@
 # WORKFLOW:
 #
 # This assumes you have Intel VTune installed and on path (using Command Line)
-# 
+#
 # 1. Profile C++ application tachyon_find_hotspots (example shipped with Intel VTune 2013):
 #
 #    amplxe-cl -collect hotspots -r result_vtune_tachyon -- ./tachyon_find_hotspots
@@ -17,7 +17,7 @@
 # 2. Export raw VTune data to csv file:
 #    ###for Intel VTune 2013
 #    amplxe-cl -R top-down -call-stack-mode all -report-out result_vtune_tachyon.csv -filter "Function Stack" -format csv -csv-delimiter comma -r result_vtune_tachyon
-#    
+#
 #    ###for Intel VTune 2015 & 2016
 #    amplxe-cl -R top-down -call-stack-mode all -column="CPU Time:Self","Module" -report-out result_vtune_tachyon.csv -filter "Function Stack" -format csv -csv-delimiter comma -r result_vtune_tachyon
 #
@@ -46,32 +46,35 @@ my $inputCSVFile = $ARGV[0];
 open(my $fh, '<', $inputCSVFile) or die "Can't read file '$inputCSVFile' [$!]\n";
 
 while (my $currLine = <$fh>){
+	# Skip log messages, e.g. "war:Column filter is ON."
+	next if $currLine =~ /^war:/;
+
 	$rowCounter = $rowCounter + 1;
 	# to discard first row which typically contains headers
 	next if $rowCounter == 1;
 	chomp $currLine;
-	#VTune - sometimes the call stack information is enclosed in double quotes (?).  To remove double quotes. 
+	#VTune - sometimes the call stack information is enclosed in double quotes (?).  To remove double quotes.
 	$currLine =~ s/\"//g;
-	
+
 	### for Intel VTune 2013
 	#$currLine =~ /(\s*)(.*),(.*),[0-9]*\.?[0-9]+[%],([0-9]*\.?[0-9]+)/ or die "Error in regular expression on the current line\n";
-	#my $func = $3.'!'.$2; 
+	#my $func = $3.'!'.$2;
 	#my $depth = length ($1);
 	#my $selfTime = $4*1000; # selfTime in msec
-	
+
 	### for Intel VTune 2015 & 2016
 	$currLine =~ /(\s*)(.*?),([0-9]*\.?[0-9]+?),(.*)/ or die "Error in regular expression on the current line $currLine\n";
-	my $func = $4.'!'.$2; 
+	my $func = $4.'!'.$2;
 	my $depth = length ($1);
 	my $selfTime = $3*1000; # selfTime in msec
-	
+
 	my $tempString = '';
 	$stack [$depth] = $func;
 	foreach my $i (0 .. $depth - 1) {
 		$tempString = $tempString.$stack[$i].";";
 	}
 	$tempString = $tempString.$func." $selfTime\n";
-	
+
 	if ($selfTime != 0){
 		print "$tempString";
 	}
